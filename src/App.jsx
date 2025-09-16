282b8acda121e15407a45ed339d0585ca0aced89
Keisean5 <seanksarreal@gmail.com>
2025-09-16 12:52:19 -0700

    Initial commit with project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav>Navbar</nav>
+      <main>All Routing</main>
+    </div>
+  );
+};
+
+export default App;

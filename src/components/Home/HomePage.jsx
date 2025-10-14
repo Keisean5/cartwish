f2721686f02a65e189a05bafc943989dca2363ce
Keisean5 <seanksarreal@gmail.com>
2025-10-14 16:33:32 -0700

    Implemented Fetching Featured Products

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -11,14 +11,14 @@ const HomePage = () => {
       <HeroSection
         title="Buy iPhone 14 Pro"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
-        link="/"
+        link="/product/68dc461765e80da93f6ac3c8"
         image={iphone}
       />
       <FeaturedProducts />
       <HeroSection
         title="Build the ultimate setup"
         subtitle="You can add Studio Display and color-matched Magic accessories to your bag after configuring your Mac mini."
-        link="/"
+        link="/product/68dc461765e80da93f6ac3d0"
         image={mac}
       />
     </div>
